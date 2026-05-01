fd141a10246f8fd016f58de0856938b5d2586136
ilyasaitbamou <ilyasaitbamou2005@gmail.com>
2026-05-01 22:11:37 +0100

    Add user seeder with admin and test user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +16,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin123'),
+            'role' => 'admin',
+            'phone' => '+212667676927',
+            'email_verified_at' => now(),
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'ilyas',
+            'email' => 'ilyas@gmail.com',
+            'password' => Hash::make('ilyas123'),
+            'role' => 'user',
+            'phone' => '+212667676927',
+            'email_verified_at' => now(),
         ]);
     }
 }
