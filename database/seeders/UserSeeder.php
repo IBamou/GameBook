68740d050394fabbd71af2c3f23491d2b2ac304c
ilyasaitbamou <ilyasaitbamou2005@gmail.com>
2026-05-01 22:20:04 +0100

    Add all seeders with proper order

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin123'),
+            'role' => 'admin',
+            'phone' => '+212667676927',
+            'email_verified_at' => now(),
+        ]);
+
+        User::create([
+            'name' => 'ilyas',
+            'email' => 'ilyas@gmail.com',
+            'password' => Hash::make('ilyas123'),
+            'role' => 'user',
+            'phone' => '+212667676927',
+            'email_verified_at' => now(),
+        ]);
+    }
+}
